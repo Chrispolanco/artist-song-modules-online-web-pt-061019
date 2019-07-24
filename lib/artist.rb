35d1cf7ae17a143e30ae24dc7e19353e1acbafab
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-24 05:34:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 class Artist
+  include Findable 
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable
-  include Findable 
   
   attr_accessor :name
   attr_reader :songs
